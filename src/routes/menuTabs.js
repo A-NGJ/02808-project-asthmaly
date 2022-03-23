9e8ea82d9a736514d876d059ec5d8c05a8924296
Livol <liv.olsen@globetracker.com>
2022-03-23 12:44:57 +0100

    Added icon for home tab

--- src/routes/menuTabs.js
+++ src/routes/menuTabs.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/FontAwesome';
 import {Home} from '../pages/Home';
 import {Visualization} from '../pages/Visualization';
 import {Profile} from '../pages/Profile';
@@ -10,7 +10,16 @@ const Tab = createBottomTabNavigator();
 export function MenuTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
       <Tab.Screen name="Visualization" component={Visualization} />
       <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
