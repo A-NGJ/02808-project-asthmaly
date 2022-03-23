cd96ba6d28b3c89d50218be2f06ece2088beae92
Aleksander Nagaj <anagaj99@gmail.com>
2022-03-23 12:05:53 +0100

    light and dark theme

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {MenuTabs} from './src/routes/menuTabs';
+import { useColorScheme } from 'react-native';
+
+import {Light, Dark} from './src/styles/style';
 
 export default function App() {
+  const scheme = useColorScheme();
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={scheme === 'dark' ? Dark : Light}>
       <MenuTabs />
     </NavigationContainer>
   );
