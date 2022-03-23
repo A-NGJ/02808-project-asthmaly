d0671af5fe8d4099612953945435d790c15b722b
Anders <anders-10@live.dk>
2022-03-23 15:06:31 +0100

    add buttons, styling for buttons and text on top of buttons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,47 @@
 import * as React from 'react';
-import {Text, View} from 'react-native';
-import { Button } from '@react-native-elements/base';
+import {Text, View, StyleSheet} from 'react-native';
+import { Button, VStack, Box } from "native-base";
 
 export function Home() {
-    return <Button title="Hello World" />;
+    // From https://docs.nativebase.io/button
+    return (
+        <View>
+            <View alignItems="center">
+                <Text>Report a Symptom</Text>
+            </View>
+            <View>
+                <Box alignItems="center">
+                    <Button key={'lg'} size={'lg'} style = {styles.top}>
+                        Track Symptom
+                    </Button>
+                </Box>
+            </View>
+            <View>
+                <Box alignItems="center">
+                    <Button key={'lg'} size={'lg'} style = {styles.top}>
+                        Track Activity
+                    </Button>
+                </Box>
+            <View>
+            </View>
+                <Box alignItems="center">
+                    <Button key={'lg'} size={'lg'} style = {styles.top}>
+                        Track Medication
+                    </Button>
+                </Box>
+            </View>
+        </View>
+    )
 }
+
+const border_radius = 6
+
+const styles = StyleSheet.create({
+    top: {
+      backgroundColor: "purple",
+      borderTopLeftRadius: border_radius,
+      borderTopRightRadius: border_radius,
+      borderBottomLeftRadius: border_radius,
+      borderBottomRightRadius: border_radius,
+    }
+  });
