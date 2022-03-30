8713cff877b9ea61f524a740f0ab8c1ac7a66ac7
Anders <anders-10@live.dk>
2022-03-30 10:48:32 +0200

    small fix

--- App.js
+++ App.js
@@ -2,10 +2,7 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {MenuTabs} from './src/routes/menuTabs';
 import { useColorScheme } from 'react-native';
-
-import {Light, Dark} from './src/styles/style';
 import { NativeBaseProvider, Text, Box } from 'native-base';
-import { useColorScheme } from 'react-native';
 import {Light, Dark} from './src/styles/style';
 
 export default function App() {
