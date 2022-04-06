2a95ec28145251646563f654fe55e2c82e367aac
Livol <liv.olsen@globetracker.com>
2022-04-06 11:33:04 +0200

    new function getting timestamp and storing data

--- src/utils/storeData.js
+++ src/utils/storeData.js
@@ -1,6 +1,24 @@
 import * as React from "react";
 
-export function storeData() {
+export function storeData(button) {
   const timestamp = new Date();
-  // code for storing it in firebase
+
+  switch (button) {
+
+    case "symptom":
+      // store user timestamp in firebase under document with symptom logs
+      break;
+
+    case "medication":
+      // store timestamp in firebase under document with medication logs
+      break;
+
+    case "activity":
+      // store timestamp in firebase under document with activity logs
+      break;
+
+    default:
+      console.error("Button type not found");
+
+  }
 }
