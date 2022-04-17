f5d12d387a7226e3eafe362ed6e52584ba522d94
Livol <liv.olsen@globetracker.com>
2022-04-17 16:18:10 +0100

    Changed style in form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,60 +1,69 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
-import { Box, Button, Center, FormControl, Heading, HStack, Input, Link, VStack } from "native-base";
+import { Box, Button, Center, FormControl, Heading, Input, Link, VStack, Icon, Stack } from "native-base";
+import MaterialIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 export function Login() {
-
+  const [show, setShow] = React.useState(false);
   return (
     <View>
-      <View style={styles.container}>
+      <View style={styles.imageContainer}>
         <Image source={require("../images/login_img.png")} style={styles.image} />
       </View>
-      <Center w="100%">
-        <Box safeArea p="2" py="8" w="90%" maxW="290">
-          <Heading size="lg" fontWeight="600" color="coolGray.800" _dark={{
-            color: "warmGray.50",
-          }}>
-            Welcome
-          </Heading>
-          <Heading mt="1" _dark={{
-            color: "warmGray.200",
-          }} color="coolGray.600" fontWeight="medium" size="xs">
-            Sign in to continue!
-          </Heading>
-
-          <VStack space={3} mt="5">
+      <Center
+        w="100%">
+        <Box
+          safeArea
+          p="2"
+          py="8"
+          w="90%"
+          maxW="290">
+          <View style={styles.headerContainer}>
+            <Heading
+              size="lg"
+              fontWeight="600"
+              color="coolGray.800">
+              Login
+            </Heading>
+          </View>
+          <VStack
+            space={3}
+            mt="5">
             <FormControl>
-              <FormControl.Label>Email ID</FormControl.Label>
-              <Input />
+              <Input
+                size="md"
+                h="50px"
+                placeholder="E-mail"
+                InputLeftElement={<Icon as={<MaterialIcons name="email" />} size={5} ml="2" color="muted.400" />}
+                style={styles.input} />
             </FormControl>
             <FormControl>
-              <FormControl.Label>Password</FormControl.Label>
-              <Input type="password" />
-              <Link _text={{
-                fontSize: "xs",
-                fontWeight: "500",
-                color: "indigo.500",
-              }} alignSelf="flex-end" mt="1">
-                Forget Password?
+              <Input
+                size="md"
+                h="50px"
+                placeholder="Password"
+                type={show ? "text" : "password"}
+                InputLeftElement={<Icon as={<MaterialIcons name="lock" />} size={5} ml="2" color="muted.400" />}
+                InputRightElement={<Icon as={<MaterialIcons name={show ? "eye" : "eye-off"} />} size={5}
+                                         mr="2" color="muted.400" onPress={() => setShow(!show)} />}
+                style={styles.input} />
+              <Link
+                _text={{
+                  fontSize: "xs",
+                  fontWeight: "500",
+                  color: "indigo.500",
+                }} alignSelf="flex-end" mt="1">
+                Forgot Password?
               </Link>
             </FormControl>
-            <Button mt="2" colorScheme="indigo">
-              Sign in
-            </Button>
-            <HStack mt="6" justifyContent="center">
-              <Text fontSize="sm" color="coolGray.600" _dark={{
-                color: "warmGray.200",
-              }}>
-                I'm a new user.{" "}
-              </Text>
-              <Link _text={{
-                color: "indigo.500",
-                fontWeight: "medium",
-                fontSize: "sm",
-              }} href="#">
-                Sign Up
-              </Link>
-            </HStack>
+            <View style={styles.buttonContainer}>
+              <Button
+                mt="2"
+                colorScheme="indigo"
+                style={styles.button}>
+                Login
+              </Button>
+            </View>
           </VStack>
         </Box>
       </Center>
@@ -63,17 +72,36 @@ export function Login() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  imageContainer: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
     marginTop: 200,
-    marginBottom: 60,
+    marginBottom: 20,
   },
   image: {
     borderRadius: 100,
     width: 180,
     height: 180,
     resizeMode: "contain",
+  },
+  headerContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 40,
+  },
+  input: {
+    borderColor: "red",
+  },
+  buttonContainer: {
+    marginTop: 40,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    borderRadius: 100,
+    width: 203.41,
+    height: 49.21,
+    backgroundColor: "#262626",
   },
 });
