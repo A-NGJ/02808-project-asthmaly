44bee064e7e3113eab91df5faf29ed351fd791b7
Livol <liv.olsen@globetracker.com>
2022-04-17 16:57:23 +0100

    Added usestate to control which screen the user should we navigated to

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { TabNavigator } from "./src/routes/Navigator";
 import { useColorScheme } from "react-native";
 import { NativeBaseProvider, Text, Box } from "native-base";
 import { Light, Dark } from "./src/styles/style";
+import { Login } from "./src/pages/Login";
 
 export default function App() {
   const scheme = useColorScheme();
+  const [loggedIn, setLoggedIn] = useState(false);
   return (
     <NativeBaseProvider>
       {/* Make the dynamic dark and light mode work again after using:
       <NavigationContainer theme={scheme === 'dark' ? Dark : Light}> */}
       <NavigationContainer theme={Dark}>
-        <TabNavigator />
+        {loggedIn ? <TabNavigator /> : <Login />}
       </NavigationContainer>
     </NativeBaseProvider>
   );
