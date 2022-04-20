f95468e90dfb0a8d328331e4e2e9346823f3e53a
Livol <liv.olsen@globetracker.com>
2022-04-20 23:57:39 +0100

    Fixed data error, showing correct data on visualisation now

--- src/utils/PlotDays.js
+++ src/utils/PlotDays.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import {StyleSheet} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
 import {
   VictoryAxis,
   VictoryBar,
@@ -8,18 +8,19 @@ import {
   VictoryLegend,
   VictoryStack,
 } from 'victory-native';
-import {darkAndBlack} from './PlotTheme';
+import { darkAndBlack } from './PlotTheme';
 import FirebaseConn from '../connection/firestore';
-import {useIsFocused} from '@react-navigation/native';
+import { useIsFocused } from '@react-navigation/native';
+import firestore from '@react-native-firebase/firestore';
 
 // Define all days and all hours for use in GetDateDays and GetDateHours
-const all_days = Array.from({length: 31}, (_, i) => (i + 1).toString());
+const all_days = Array.from({ length: 31 }, (_, i) => (i + 1).toString());
 
 function make_data_helper(dates, x_name, y_name) {
   let datalist = [];
   for (const key in dates) {
     const value = dates[key];
-    const observation = {[x_name]: key, [y_name]: value};
+    const observation = { [x_name]: key, [y_name]: value };
     datalist.push(observation);
   }
   return datalist;
@@ -27,21 +28,22 @@ function make_data_helper(dates, x_name, y_name) {
 
 // Function to output an object with days as properties and number of observations for each day as values
 function GetDateDays(dates) {
-  let datetimes_byday = {};
+  let dateTimes = {};
 
   // Initialize all day values
   for (let all_day in all_days) {
     // For some reason, this fix is needed even though the values go from 1 to 31 already
     all_day = parseInt(all_day) + 1;
-    datetimes_byday[all_day.toString()] = 0;
+    dateTimes[all_day.toString()] = 0;
   }
 
   for (let i = 0; i < dates.length; i++) {
-    const day = dates[i].getDate().toString(); // Get day number and convert to string
+    // Get day number and convert to string
+    const day = dates[i].getDate().toString();
     // Increment if day has been observed
-    datetimes_byday[day] += 1;
+    dateTimes[day] += 1;
   }
-  return make_data_helper(datetimes_byday, 'Days', 'Count');
+  return make_data_helper(dateTimes, 'Days', 'Count');
 }
 
 // Get the number of days in a certain month for x-axis in plot
@@ -50,17 +52,17 @@ function daysInMonth(month, year) {
 }
 
 const datetimes_byhour2 = [
-  {Hours: 1, Count: 1},
-  {Hours: 13, Count: 1},
-  {Hours: 14, Count: 1},
-  {Hours: 18, Count: 1},
+  { Hours: 1, Count: 1 },
+  { Hours: 13, Count: 1 },
+  { Hours: 14, Count: 1 },
+  { Hours: 18, Count: 1 },
 ];
 
 const datetimes_byhour3 = [
-  {Hours: 1, Count: 2},
-  {Hours: 2, Count: 2},
-  {Hours: 3, Count: 2},
-  {Hours: 4, Count: 2},
+  { Hours: 1, Count: 2 },
+  { Hours: 2, Count: 2 },
+  { Hours: 3, Count: 2 },
+  { Hours: 4, Count: 2 },
 ];
 
 function getSymptoms() {
@@ -73,7 +75,7 @@ function getSymptoms() {
       const symptoms = await firebaseConn.getSymptoms();
       setTimestamps(symptoms);
     };
-    fetchFirebase();
+    return () => fetchFirebase();
   }, [isFocused]);
 
   const dateTimes = timestamps.map(x => new Date(x));
@@ -83,7 +85,7 @@ function getSymptoms() {
 
 export function plotDays(figsize_x, figsize_y) {
   const barRatio = 1.0;
-  const dateTimesByDay = getSymptoms();
+  const dateTimeByDay = getSymptoms();
 
   return (
     <VictoryChart
@@ -91,18 +93,18 @@ export function plotDays(figsize_x, figsize_y) {
       theme={darkAndBlack}
       width={figsize_x}
       height={figsize_y}
-      padding={{top: 40, bottom: 80, left: 50, right: 120}}>
+      padding={{ top: 40, bottom: 80, left: 50, right: 120 }}>
       <VictoryAxis tickValues={[1, 2, 3, 4]} tickFormat={all_days} />
       <VictoryAxis
         dependentAxis
         style={{
-          grid: {stroke: '#F4F5F7', strokeWidth: 1},
+          grid: { stroke: '#F4F5F7', strokeWidth: 1 },
         }}
         tickFormat={x => `${x}`}
       />
       <VictoryStack>
         <VictoryBar
-          data={dateTimesByDay}
+          data={dateTimeByDay}
           x="Days"
           y="Count"
           barRatio={barRatio}
@@ -121,12 +123,12 @@ export function plotDays(figsize_x, figsize_y) {
         />
       </VictoryStack>
       <VictoryLegend
-        data={[{name: 'Exercise'}, {name: 'Medication'}, {name: 'Symptom'}]}
+        data={[{ name: 'Exercise' }, { name: 'Medication' }, { name: 'Symptom' }]}
         title="Status"
         labelComponent={<VictoryLabel angle={0} />}
         x={figsize_x - 115}
         y={35}
-        rowGutter={{top: 0, bottom: -5}}
+        rowGutter={{ top: 0, bottom: -5 }}
       />
     </VictoryChart>
   );
