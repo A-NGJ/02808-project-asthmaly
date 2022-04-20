66dcf7e05cd5a7fe84157ed9c043cb0e2a1f98e2
Livol <liv.olsen@globetracker.com>
2022-04-21 00:04:20 +0100

    Added export to GetDateHours function and automatic lint fix

--- src/utils/PlotHours.js
+++ src/utils/PlotHours.js
@@ -1,14 +1,20 @@
 import * as React from 'react';
 // import * as assign from 'assign';
 import {StyleSheet} from 'react-native';
-import { VictoryBar, VictoryChart, VictoryStack, VictoryAxis, VictoryLegend, VictoryLabel} from "victory-native";
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryStack,
+  VictoryAxis,
+  VictoryLegend,
+  VictoryLabel,
+} from 'victory-native';
 import {darkAndBlack} from './PlotTheme';
-
 
 // Define all hours for use in GetDateHours
 const all_hours = Array.from({length: 24}, (_, i) => (i + 1).toString());
-// console.log(all_days);
 
+// console.log(all_days);
 
 function make_data_helper(dates, x_name, y_name) {
   var datalist = [];
@@ -17,14 +23,13 @@ function make_data_helper(dates, x_name, y_name) {
     var observation = {[x_name]: key, [y_name]: value};
     datalist.push(observation);
   }
-  return datalist
+  return datalist;
 }
 
-
 // Function to output an object with hours as properties and number of observations for each hour as values
-function GetDateHours(dates) {
+export function GetDateHours(dates) {
   var datetimes_byhour = new Object();
-  
+
   // Initialize all hour values
   for (var all_hour in all_hours) {
     // For some reason, this fix is needed even though the values go from 1 to 24 already
@@ -33,45 +38,51 @@ function GetDateHours(dates) {
   }
 
   for (var i = 0; i < datetimes.length; i++) {
-    var hour = datetimes[i].getHours().toString();  // Get hour and convert to string
-    
+    var hour = datetimes[i].getHours().toString(); // Get hour and convert to string
+
     // Increment if hour has been observed
     datetimes_byhour[hour] += 1;
   }
   // console.log(datetimes_byhour);
-  var hour_data = make_data_helper(datetimes_byhour, "Hours", "Count");
-  return hour_data
+  var hour_data = make_data_helper(datetimes_byhour, 'Hours', 'Count');
+  return hour_data;
 }
-
 
 // How to generate the date of when the button was pressed. Contains year, month, day, hours, minutes and seconds. Maybe also timezone
 // const datetime = [new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes(), now.getSeconds())]
-const datetimes = [new Date(1618420117245), new Date(1618420717245), new Date(1618421317245), new Date(1618430117245),
-                   new Date(1618620117245), new Date(1618720717245), new Date(1618821317245), new Date(1618930117245)]
-
+const datetimes = [
+  new Date(1618420117245),
+  new Date(1618420717245),
+  new Date(1618421317245),
+  new Date(1618430117245),
+  new Date(1618620117245),
+  new Date(1618720717245),
+  new Date(1618821317245),
+  new Date(1618930117245),
+];
 
 // Get the number of days in a certain month for x-axis in plot
-function daysInMonth (month, year) {
+function daysInMonth(month, year) {
   return new Date(year, month, 0).getDate();
 }
 
 const datetimes_byhour = GetDateHours(datetimes);
 // console.log("The hours:")
-console.log(datetimes_byhour);
+//console.log(datetimes_byhour);
 
 const datetimes_byhour2 = [
-    {Hours: 1, Count: 2},
-    {Hours: 13, Count: 3},
-    {Hours: 14, Count: 4},
-    {Hours: 18, Count: 5}
-  ];
+  {Hours: 1, Count: 2},
+  {Hours: 13, Count: 3},
+  {Hours: 14, Count: 4},
+  {Hours: 18, Count: 5},
+];
 
 const datetimes_byhour3 = [
-    {Hours: 1, Count: 1},
-    {Hours: 2, Count: 1},
-    {Hours: 3, Count: 1},
-    {Hours: 4, Count: 1}
-  ];
+  {Hours: 1, Count: 1},
+  {Hours: 2, Count: 1},
+  {Hours: 3, Count: 1},
+  {Hours: 4, Count: 1},
+];
 
 // Sort the data point by hour and by date and insert them into an object for easy plotting.
 
@@ -97,7 +108,7 @@ const datetimes_byhour3 = [
 //   {quarter: 4, earnings: 15500}
 // ];
 
-var barRatio = 1.0
+var barRatio = 1.0;
 
 export function plotHours(figsize_x, figsize_y) {
   return (
@@ -106,18 +117,14 @@ export function plotHours(figsize_x, figsize_y) {
       theme={darkAndBlack}
       width={figsize_x}
       height={figsize_y}
-      padding={{ top: 40, bottom: 80, left: 50, right: 120 }}
-    >
-      <VictoryAxis
-        tickValues={[1, 2, 3, 4]}
-        tickFormat={all_hours}
-      />
+      padding={{top: 40, bottom: 80, left: 50, right: 120}}>
+      <VictoryAxis tickValues={[1, 2, 3, 4]} tickFormat={all_hours} />
       <VictoryAxis
         dependentAxis
         style={{
-          grid: { stroke: '#F4F5F7', strokeWidth: 1 },
+          grid: {stroke: '#F4F5F7', strokeWidth: 1},
         }}
-        tickFormat={(x) => (`${x}`)}
+        tickFormat={x => `${x}`}
       />
       <VictoryStack>
         <VictoryBar
@@ -141,24 +148,22 @@ export function plotHours(figsize_x, figsize_y) {
         />
       </VictoryStack>
       <VictoryLegend
-        data={[
-          { name: "Exercise" }, { name: "Medication" }, { name: "Symptom" }
-        ]}
+        data={[{name: 'Exercise'}, {name: 'Medication'}, {name: 'Symptom'}]}
         title="Status"
-        labelComponent={<VictoryLabel angle={0}/>}
-        x={figsize_x-115} y={35}
-        rowGutter={{ top: 0, bottom: -5 }}
+        labelComponent={<VictoryLabel angle={0} />}
+        x={figsize_x - 115}
+        y={35}
+        rowGutter={{top: 0, bottom: -5}}
       />
     </VictoryChart>
-  )
+  );
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#383434"
-  }
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#383434',
+  },
 });
