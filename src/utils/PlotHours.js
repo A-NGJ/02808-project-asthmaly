4d813e2f694d4ffd9d520f01c2ffb5d2351b2981
Livol <liv.olsen@globetracker.com>
2022-04-21 20:09:26 +0100

    Removed unused data and corrected function

--- src/utils/PlotHours.js
+++ src/utils/PlotHours.js
@@ -36,8 +36,8 @@ export function GetDateHours(dates) {
     dateTimes[all_hour.toString()] = 0;
   }
 
-  for (let i = 0; i < datetimes.length; i++) {
-    const hour = datetimes[i].getHours().toString(); // Get hour and convert to string
+  for (let i = 0; i < dates.length; i++) {
+    const hour = dates[i].getHours().toString(); // Get hour and convert to string
 
     // Increment if hour has been observed
     dateTimes[hour] += 1;
@@ -45,39 +45,10 @@ export function GetDateHours(dates) {
   return make_data_helper(dateTimes, 'Hours', 'Count');
 }
 
-// How to generate the date of when the button was pressed. Contains year, month, day, hours, minutes and seconds. Maybe also timezone
-// const datetime = [new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes(), now.getSeconds())]
-const datetimes = [
-  new Date(1618420117245),
-  new Date(1618420717245),
-  new Date(1618421317245),
-  new Date(1618430117245),
-  new Date(1618620117245),
-  new Date(1618720717245),
-  new Date(1618821317245),
-  new Date(1618930117245),
-];
-
 // Get the number of days in a certain month for x-axis in plot
 function daysInMonth(month, year) {
   return new Date(year, month, 0).getDate();
 }
-
-const datetimes_byhour = GetDateHours(datetimes);
-
-const datetimes_byhour2 = [
-  {Hours: 1, Count: 2},
-  {Hours: 13, Count: 3},
-  {Hours: 14, Count: 4},
-  {Hours: 18, Count: 5},
-];
-
-const datetimes_byhour3 = [
-  {Hours: 1, Count: 1},
-  {Hours: 2, Count: 1},
-  {Hours: 3, Count: 1},
-  {Hours: 4, Count: 1},
-];
 
 export function plotHours(figsize_x, figsize_y) {
   const barRatio = 1.0;
