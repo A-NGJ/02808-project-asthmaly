44da0d9a4fc10175614a3b270c0c1f6734d02552
Aleksander Nagaj <anagaj99@gmail.com>
2022-04-27 10:07:54 +0200

    add modal with activity type choice

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React, {useState, useEffect} from "react";
-import { Text, View, StyleSheet, Image, Dimensions } from "react-native";
+import { Text, View, StyleSheet, Image, Dimensions, Modal, Pressable} from "react-native";
 import { Button, Box } from "native-base";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import {Obs} from '../constants/constants';
 import FirebaseConn from '../connection/firestore';
 import { useIsFocused } from "@react-navigation/native";
+import { useTheme } from "@react-navigation/native";
+import { RadioButton } from "../components/buttons";
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -32,9 +34,32 @@ export function Home(){
   const firebaseConn = FirebaseConn.getInstance();
     // From https://docs.nativebase.io/button
   const isFocused = useIsFocused();
+  const { colors } = useTheme();
 
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const PROP = [
+    {
+      key: "cycling",
+      name: "Cycling",
+    },
+    {
+      key: "walking",
+      name: "Walking",
+    },
+    {
+      key: "running",
+      name: "Running",
+    },
+    {
+      key: "other",
+      name: "Other",
+    }
+  ];
+
   useEffect(() => {
     const fetchFirebase = async () => {
       const name = await firebaseConn.getName();
@@ -47,6 +72,42 @@ export function Home(){
 
     return (
     <View style={{height: windowHeight, width: windowWidth, flex: 1, paddingBottom: 10}}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(false);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View 
+            style={styles.modalView}
+            backgroundColor={colors.background}
+          >
+            <View style={{paddingTop: 20}}>
+              <RadioButton PROP={PROP} theme={colors}/>
+            </View>
+            <View style={{padding: 20, flex: 1}}>
+              <Pressable
+                style={({ pressed }) => [
+                  styles.button,
+                  {
+                    backgroundColor: pressed
+                    ? colors.buttonActive
+                    : colors.buttonInactive
+                  }
+                ]}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <View style={styles.centeredView}>
+                  <Text style={styles.maintext}>Save</Text>
+                </View>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
       {/* Profile info */}
       <View style = {{top: '5%', justifyContent: 'center', alignItems: 'center'}}>
         <Image source={require('../images/anne_nielsen_profile_picture.png')} style={styles.profilePicture} />
@@ -56,7 +117,7 @@ export function Home(){
       </View>
       <View style = {{top: '5%', justifyContent: 'center', alignItems: 'center'}}>
         <Text style={styles.emailText}>{email}</Text>
-    </View>
+      </View>
 
     {/* Buttons and text fields */}
     <View style={{ top: "21%", justifyContent: "center", alignItems: "center" }}>
@@ -69,54 +130,54 @@ export function Home(){
     {/* Track a symptom button */}
     <View style={{flex: 1}}>
     <View>
-        <Box alignItems="center">
-            <Button
-              key={'lg'} bg="#383434" size={'lg'}
-              style = {styles.button} _pressed={{bg: "gray.800"}}
-              onPress={() => firebaseConn.addObs(Obs.SYMPTOMS)}
-            >
-                <View style={{flexDirection:'row', alignItems:'center', justifyContent:'center'}}>
-                    <View style={{margin: 0}}>
-                        <Image source={require('../images/symptom_icon.png')} style={styles.iconImage}/>
-                    </View>
-                    <View>
-                        <Text style={styles.buttonHeader}>
-                            Track Symptom
-                        </Text>
-                        <Text style={styles.buttonFooter}>
-                            Overall Asthma Symptoms
-                        </Text>
-                    </View>
-                </View>
-            </Button>
-        </Box>
+      <Box alignItems="center">
+        <Button
+          key={'lg'} bg="#383434" size={'lg'}
+          style = {styles.button} _pressed={{bg: "gray.800"}}
+          onPress={() => firebaseConn.addObs(Obs.SYMPTOMS)}
+        >
+          <View style={{flexDirection:'row', alignItems:'center', justifyContent:'center'}}>
+            <View style={{margin: 0}}>
+              <Image source={require('../images/symptom_icon.png')} style={styles.iconImage}/>
+            </View>
+            <View>
+              <Text style={styles.buttonHeader}>
+                Track Symptom
+              </Text>
+              <Text style={styles.buttonFooter}>
+                Overall Asthma Symptoms
+              </Text>
+            </View>
+          </View>
+        </Button>
+      </Box>
     </View>
     <View style = {{marginTop: '5%'}}>
     </View>
 
-    {/* Track medication button */}
-    <View>
+      {/* Track medication button */}
+      <View>
         <Box alignItems="center">
-            <Button key={'lg'} bg="#383434" size={'lg'}
-              style = {styles.button} _pressed={{bg: "gray.800"}}
-              onPress={() => firebaseConn.addObs(Obs.MEDICATION)}
-            >
-                <View style={{flexDirection:'row', alignItems:'center', justifyContent:'center'}}>
-                    <View style={{margin: 0}}>
-                        <Image source={require('../images/medication_icon.png')} style={styles.iconImage}/>
-                    </View>
-                    <View>
-                    <Text style={styles.buttonHeader}>
-                        Track Medication
-                    </Text>
-                    <Text style={styles.buttonFooter}>
-                        Preventative or Acute Medication
-                    </Text>
-                    </View>
-                </View>
-            </Button>
+          <Button key={'lg'} bg="#383434" size={'lg'}
+            style = {styles.button} _pressed={{bg: "gray.800"}}
+            onPress={() => firebaseConn.addObs(Obs.MEDICATION)}
+          >
+            <View style={{flexDirection:'row', alignItems:'center', justifyContent:'center'}}>
+              <View style={{margin: 0}}>
+                <Image source={require('../images/medication_icon.png')} style={styles.iconImage}/>
+              </View>
+              <View>
+                <Text style={styles.buttonHeader}>
+                  Track Medication
+                </Text>
+                <Text style={styles.buttonFooter}>
+                  Preventative or Acute Medication
+                </Text>
+              </View>
+            </View>
+          </Button>
         </Box>
-    </View>
+      </View>
     </View>
     {/* <View style = {{marginTop: '8%'}}>
     </View> */}
@@ -135,7 +196,10 @@ export function Home(){
             size={"lg"}
             style={styles.button}
             _pressed={{ bg: "gray.800" }}
-            onPress={() => firebaseConn.addObs(Obs.ACTIVITY)}>
+            onPress={() => {
+              setModalVisible(true)
+              // firebaseConn.addObs(Obs.ACTIVITY)
+            }}> 
             <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
               <View style={{ margin: 0 }}>
                 <Image source={require("../images/exercise_icon.png")} style={styles.iconImage} />
@@ -156,27 +220,23 @@ export function Home(){
   );
 }
 
-const border_radius = 6;
+const BORDER_RADIUS = 6;
 
 const styles = StyleSheet.create({
   // General button design
   button: {
-    //   backgroundColor: "#383434",
-    // width: 350,
     width: "90%",
     aspectRatio: 350 / 75,
-    borderTopLeftRadius: border_radius,
-    borderTopRightRadius: border_radius,
-    borderBottomLeftRadius: border_radius,
-    borderBottomRightRadius: border_radius,
+    borderTopLeftRadius: BORDER_RADIUS,
+    borderTopRightRadius: BORDER_RADIUS,
+    borderBottomLeftRadius: BORDER_RADIUS,
+    borderBottomRightRadius: BORDER_RADIUS,
   },
 
   // Heading text
   maintext: {
     fontSize: 20,
-    // fontFamily: "roboto",
     color: "white",
-    // fontWeight: "bold",
   },
 
   // Button header
@@ -185,7 +245,6 @@ const styles = StyleSheet.create({
     aspectRatio: 240 / 30,
     fontSize: 15,
     left: "5%",
-    // fontFamily: "roboto",
     color: "white",
     fontWeight: "bold",
   },
@@ -196,7 +255,6 @@ const styles = StyleSheet.create({
     aspectRatio: 240 / 20,
     fontSize: 15,
     left: "5%",
-    // fontFamily: "roboto",
     color: "white",
   },
 
@@ -204,7 +262,6 @@ const styles = StyleSheet.create({
   emailText: {
     fontSize: 12,
     color: "gray",
-    // fontFamily: "roboto",
   },
 
   // Add profile picture
@@ -222,4 +279,17 @@ const styles = StyleSheet.create({
     left: 0,
   },
 
+  modalView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: windowHeight*0.24,
+    marginHorizontal: windowWidth*0.05
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    // paddingTop: 22,
+  }
 });
